bbc62c0703ba8224d0d7876779f40611c7e0c368
Oksanaka848 <okmitka@mail.ru>
2022-11-20 19:07:36 +0300

    end

---
+++ cat.js
@@ -0,0 +1,21 @@
+/*function deRequireCb(elClass) {
+    el=document.getElementsByClassName(elClass);
+
+    var atLeastOneChecked=false;
+    for (i=0; i<el.length; i++) {
+        if (el[i].checked === true) {
+            atLeastOneChecked=true;
+        }
+    }
+
+    if (atLeastOneChecked === true) {
+        for (i=0; i<el.length; i++) {
+            el[i].required = false;
+        }
+    } else {
+        for (i=0; i<el.length; i++) {
+            el[i].required = true;
+        }
+    }
+}
+*/
