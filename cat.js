53b9df65ae89732859a5a721e11dc87c3fbdcfb1
Oksanaka848 <okmitka@mail.ru>
2022-11-25 22:07:28 +0300

    ch

--- cat.js
+++ cat.js
@@ -19,14 +19,44 @@
     }
 }
 */
-let button = document.getElementById ('button');
-button.addEventListener("click", function() {
-    check();
-    
-  });
-  function check() {
-    let checkd = document.querySelectorAll('input[type="checkbox"]');
-if (checkd.checked.length > 0)
-return true
-}
+/*Напишите класс Cat со свойствами, соответствующими полям вашего опросника (например, кличка, корм и пр.), 
+и по нажатию на кнопку "Ок" создайте экземпляр этого класса, 
+заполнив его свойства ответами из опросника. 
+В этом классе будут только свойства и конструктор, без методов. 
+Выведите результат (заполненный объект класса Cat) в консоль при помощи console.log.*/
 
+/*let button = document.getElementById ('button');
+button.addEventListener("click", function() {
+    check()
+      });
+  function check() {
+    let checkd = document.querySelectorAll('input[type="checkbox"]:checked');
+if (checkd.checked == false)
+return false
+else{
+alert('выберите хотя бы один вариант')
+}
+}*/
+class Cat {
+    constructor (name, breeds, food, sex){
+    this.name = name;
+    this.breeds = breeds;
+    this.food = food;
+    this.sex = sex;
+    }
+   
+}
+   
+    
+        let name2 = document.getElementById ('name').value;
+        //let breeds2 = document.querySelector('select[name="breeds"].checked').nextElementSibling.textContent;
+        let food2 = document.querySelectorAll('input[type="checkbox"]:checked').nextElementSibling.textContent;
+        let sex2 = document.querySelector('input[type="radio"]:checked').value;
+
+    let mycat = new Cat(name2, breeds2, food2, sex2 );
+    
+   console.log(mycat);
+   let button = document.getElementById ('button');
+   button.addEventListener("click", console.log(mycat)
+         );
+  
