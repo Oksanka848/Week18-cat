8ead554f4c904dfb4083ffbf59bb179a1c88513c
Oksanaka848 <okmitka@mail.ru>
2022-12-07 20:16:42 +0300

    ch

--- cat.js
+++ cat.js
@@ -1,37 +1,3 @@
-/*function deRequireCb(elClass) {
-    el=document.getElementsByClassName(elClass);
-
-    var atLeastOneChecked=false;
-    for (i=0; i<el.length; i++) {
-        if (el[i].checked === true) {
-            atLeastOneChecked=true;
-        }
-    }
-
-    if (atLeastOneChecked === true) {
-        for (i=0; i<el.length; i++) {
-            el[i].required = false;
-        }
-    } else {
-        for (i=0; i<el.length; i++) {
-            el[i].required = true;
-        }
-    }
-}
-let user = {
-        mycatname: document.getElementById("name").value,
-breed : document.getElementById("breeds").value,
-catfoods : Array.from(document.querySelectorAll('input[name="food"]:checked')),
-mycatfood : catfoods.map((item) => item.value),
-mycatsex : document.querySelectorAll('input[name="sex"]:checked').value
-    }
-*/
-/*Напишите класс Cat со свойствами, соответствующими полям вашего опросника (например, кличка, корм и пр.), 
-и по нажатию на кнопку "Ок" создайте экземпляр этого класса, 
-заполнив его свойства ответами из опросника. 
-В этом классе будут только свойства и конструктор, без методов. 
-Выведите результат (заполненный объект класса Cat) в консоль при помощи console.log.*/
-
 let button = document.getElementById ('button');
 let mycatname = document.getElementById("name").value;
 let breed = document.getElementById("breeds").value;
@@ -65,24 +31,3 @@ class Cat {
 let mycat = new Cat(mycatname, breed, mycatfood, mycatsex);
 
 console.log(mycat);
-
-button.addEventListener("click", function(e) {
-   
-    e.preventDefault();
-    const form =document.querySelector('form');
-    
-    fetch('https://httpbin.org/post',
-    {
-        method: 'POST',
-        body: new FormData(form),
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
-        },
-    })
-    .then(response=>response.json())
-    .then(form => {
-        console.log(form);
-    })
-.catch(error => console.log(error));
-
-    });
---
+++ cat2.js
@@ -0,0 +1,88 @@
+/*function deRequireCb(elClass) {
+    el=document.getElementsByClassName(elClass);
+
+    var atLeastOneChecked=false;
+    for (i=0; i<el.length; i++) {
+        if (el[i].checked === true) {
+            atLeastOneChecked=true;
+        }
+    }
+
+    if (atLeastOneChecked === true) {
+        for (i=0; i<el.length; i++) {
+            el[i].required = false;
+        }
+    } else {
+        for (i=0; i<el.length; i++) {
+            el[i].required = true;
+        }
+    }
+}
+let user = {
+        mycatname: document.getElementById("name").value,
+breed : document.getElementById("breeds").value,
+catfoods : Array.from(document.querySelectorAll('input[name="food"]:checked')),
+mycatfood : catfoods.map((item) => item.value),
+mycatsex : document.querySelectorAll('input[name="sex"]:checked').value
+    }
+*/
+/*Напишите класс Cat со свойствами, соответствующими полям вашего опросника (например, кличка, корм и пр.), 
+и по нажатию на кнопку "Ок" создайте экземпляр этого класса, 
+заполнив его свойства ответами из опросника. 
+В этом классе будут только свойства и конструктор, без методов. 
+Выведите результат (заполненный объект класса Cat) в консоль при помощи console.log.*/
+
+let button = document.getElementById ('button');
+let mycatname = document.getElementById("name").value;
+let breed = document.getElementById("breeds").value;
+let catfoods = Array.from(document.querySelectorAll('input[name="food"]:checked'));
+let mycatfood = catfoods.map((item) => item.value);
+let catsex = Array.from(document.querySelectorAll('input[name="sex"]:checked'));
+let mycatsex = catsex.map((item) => item.value);
+button.addEventListener("click", function() {
+    check()
+    console.log(mycat)
+      });
+  function check() {
+    document.getElementById ('error').innerHTML=" ";
+    
+if (catfoods.length == 0){
+    document.getElementById ('error').innerHTML+= "выберите хотя бы один вариант <br>";
+
+}}
+
+class Cat {
+  constructor(name, breeds, food, sex) {
+    this.name = name;
+    this.breeds = breeds;
+    this.food = food;
+    this.sex = sex;
+  }
+}
+
+
+
+let mycat = new Cat(mycatname, breed, mycatfood, mycatsex);
+
+console.log(mycat);
+
+button.addEventListener("click", function(e) {
+   
+    e.preventDefault();
+    const form =document.querySelector('form');
+    
+    fetch('https://httpbin.org/post',
+    {
+        method: 'POST',
+        body: new FormData(form),
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+    })
+    .then(response=>response.json())
+    .then(form => {
+        console.log(form);
+    })
+.catch(error => console.log(error));
+
+    });
