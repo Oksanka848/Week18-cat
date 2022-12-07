69f2a2276c56b20dee0d7cce380e037a639d062b
Oksanaka848 <okmitka@mail.ru>
2022-12-07 22:21:23 +0300

    ch

--- cat.js
+++ cat.js
@@ -1,3 +1,4 @@
+//let form =  document.getElementById ('form');
 let button = document.getElementById ('button');
 let mycatname = document.getElementById("name").value;
 let breed = document.getElementById("breeds").value;
@@ -5,10 +6,11 @@ let catfoods = Array.from(document.querySelectorAll('input[name="food"]:checked'
 let mycatfood = catfoods.map((item) => item.value);
 let catsex = Array.from(document.querySelectorAll('input[name="sex"]:checked'));
 let mycatsex = catsex.map((item) => item.value);
-button.addEventListener("click", function() {
+button.onclick = function() {
     check()
     console.log(mycat)
-      });
+      }
+      
   function check() {
     document.getElementById ('error').innerHTML=" ";
     
@@ -16,7 +18,6 @@ if (catfoods.length == 0){
     document.getElementById ('error').innerHTML+= "выберите хотя бы один вариант <br>";
 
 }}
-
 class Cat {
   constructor(name, breeds, food, sex) {
     this.name = name;
@@ -25,9 +26,23 @@ class Cat {
     this.sex = sex;
   }
 }
-
-
-
 let mycat = new Cat(mycatname, breed, mycatfood, mycatsex);
-
 console.log(mycat);
+button.addEventListener('click' , function(event) {
+   
+    event.preventDefault();
+    fetch('https://httpbin.org/post',
+    {
+        method: 'POST',
+        body: new FormData(form),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+    })
+    .then(response => response.json())
+    .then(user => {
+        console.log(user);
+    })
+.catch(error => console.log(error));
+});
+
